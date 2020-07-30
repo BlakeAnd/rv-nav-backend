5f69ad6a12e1bd690aa6a375af1eba1e9aa12d3d
Blake Mitchell <mblakeandrew@gmail.com>
2020-07-30 14:54:09 -0700

    changed DB_ENV to NODE_ENV

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -2,7 +2,6 @@ const knex = require('knex')
 
 const knexconfig = require('../knexfile.js')
 
-const env = process.env.DB_ENV || "development"
-console.log("dee ee", process.env.DB_ENV);
-console.log("env", env);
+const env = process.env.NODE_ENV || "development"
+
 module.exports = knex(knexconfig[env])
