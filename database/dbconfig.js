78faf3d10f80c961fbbfd422f011f0b375760325
Blake Mitchell <mblakeandrew@gmail.com>
2020-07-29 19:40:35 -0700

    log

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -4,4 +4,5 @@ const knexconfig = require('../knexfile.js')
 
 const env = process.env.DB_ENV || "development"
 console.log("dee ee", process.env.DB_ENV);
+console.log("env", env);
 module.exports = knex(knexconfig[env])
